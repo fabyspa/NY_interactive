31fdaa043954685fd0ced7e9c2dcd475a4da8de6
fabyspa <fabyyy.spa@gmail.com>
2023-03-06 17:04:02 +0100

    InfoAggiuntive
    Da aggiungere le traduzioni?

--- NY_interactive/Assets/Script/GameObjectListBoxRiserva.cs
+++ NY_interactive/Assets/Script/GameObjectListBoxRiserva.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 namespace AirFishLab.ScrollingList.Demo
 {
     public class GameObjectListBoxRiserva : ListBox
@@ -10,6 +10,8 @@ namespace AirFishLab.ScrollingList.Demo
         [SerializeField]
         private Image _image;
         private Sprite tex;
+        [SerializeField]
+        private GameObject infos;
         
         protected override void UpdateDisplayContent(object content)
         {
@@ -19,7 +21,16 @@ namespace AirFishLab.ScrollingList.Demo
             _image.sprite = UpdateImage(dataWrapper.data.name);
             _nameENG.text = dataWrapper.data.name_eng;
             _descrENG.text = dataWrapper.data.descr_eng;
-            
+            //regione,luogo,anno istituzione,superficie,reparto di competenza
+            infos.transform.GetChild(0).GetComponentInChildren<Text>().text = dataWrapper.data.region;
+            infos.transform.GetChild(1).GetComponentInChildren<Text>().text = dataWrapper.data.luogo;
+            infos.transform.GetChild(2).GetComponentInChildren<Text>().text = dataWrapper.data.anno;
+            infos.transform.GetChild(3).GetComponentInChildren<Text>().text = dataWrapper.data.sup;
+            infos.transform.GetChild(4).GetComponentInChildren<Text>().text = "reparto di competena da aggiungere";
+
+
+
+
         }
         public Sprite UpdateImage(string _name)
         {
